644fe666f4ded1d3e0c47293095f97113d23b5b2
Ravi <wolfsbane9513@gmail.com>
2023-08-19 15:05:14 +0530

    output logs

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,13 +74,13 @@ if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
             response = generate_llama2_response(prompt)
-            logger.info(f"user prompt and api response {prompt},{response}")
             placeholder = st.empty()
             full_response = ''
             for item in response:
                 full_response += item
                 placeholder.markdown(full_response)
             placeholder.markdown(full_response)
+        logger.info(f"user prompt and api response {prompt},{full_response}")
         message = {"role":"assistant","content": full_response}
         st.session_state.messages.append(message) 
 
