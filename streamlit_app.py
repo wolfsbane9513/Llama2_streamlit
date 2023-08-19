4e807a56822e815cb4a8843044824af3c6cf7ff7
Ravi <wolfsbane9513@gmail.com>
2023-08-19 15:16:12 +0530

    debug changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,6 +56,7 @@ def generate_llama2_response(prompt_input):
             string_dialogue += "User" + dict_message["content"] + "\\n\\n"
         else:
             string_dialogue += "Assistant" + dict_message["content"] + "\\n\\n"
+    logger.info(f"string dialogue{string_dialogue} - {prompt_input}")
     output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5',
                            input = {"prompt": f"{string_dialogue}{prompt_input} Assistant: ",
                                     "temperature":temperature, "top_p":top_p, "max_length":max_length, "repetition_penalty":1})
